09e99b7a5264e554cace6e1f230149929f616b8c
github cjl248 <github cjl248@cornell.edu>
2019-08-06 11:48:55 -0400

    Done.

--- spec/basic_sinatra_forms_spec.rb
+++ spec/basic_sinatra_forms_spec.rb
@@ -9,8 +9,8 @@ describe App do
     it 'renders basketball team form' do
       visit '/newteam'
       expect(page).to have_selector("form")
-      expect(page).to have_field(:name)
-      expect(page).to have_field(:coach)
+      expect(page).to have_field()
+      expect(page).to have_field()
       expect(page).to have_field(:pg)
       expect(page).to have_field(:sg)
       expect(page).to have_field(:sf)
